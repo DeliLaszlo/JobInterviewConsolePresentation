82d7b756c2e09e95ed9c0e6beae15026c63e8e86
Deli Laszlo <deli.laszlo.viktor@hallgato.sze.hu>
2026-04-19 13:30:41 +0200

    Add project files.

---
+++ JobInterviewConsolePresentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JobInterviewConsolePresentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
